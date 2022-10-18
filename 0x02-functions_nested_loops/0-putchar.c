ec59493b728348837b93543431f4469557521d74
midealabi <ayoomide@yahoomail.com>
2022-10-18 06:32:52 -0700

    my first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include 'main.h'
+#include <main.h>
 
 /**
  * main - print _putchar
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	putchar('_');
+	char putchar('_');
 	putchar('p');
 	putchar('u');
 	putchar('t');
