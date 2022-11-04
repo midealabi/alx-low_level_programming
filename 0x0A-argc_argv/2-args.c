d511193644bc8a6ddf2231e28329f6ae5057318f
midealabi <ayoomide@yahoomail.com>
2022-11-04 08:13:54 -0700

    ayo

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,14 +9,13 @@
 
 int main(int argc, char *argv[])
 {
-	int count = 0
-		if (argc > 0)
-		{
-			while (count < argc)
-			{
-				printf("%s\n", argv[count]);
-				count++;
-			}
-		}
+	int i  = 0;
+
+	while (argc--)
+	{
+		printf("%s\n" , argv[i]);
+		i++;
+	}
 	return (0);
 }
+
